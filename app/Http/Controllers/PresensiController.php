748039806cf949086596da649a2bf3897a160ed2
novadityap <novaaditya23@gmail.com>
2023-10-27 09:51:58 +0700

    Mengubah dan menambahkan presensi datang dan pulang pada user

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -11,30 +11,47 @@ use Illuminate\Http\Request;
 class PresensiController extends Controller
 {
   public function index() {
-    $waktu = Waktu::findOrFail(1);
+    $waktuBiasa = Waktu::where('hari', 'biasa')->first();
+    $waktuJumat = Waktu::where('hari', 'jumat')->first();
+    $waktuBukaBiasa = Carbon::parse($waktuBiasa->buka);
+    $waktuTutupBiasa = Carbon::parse($waktuBiasa->tutup);
+    $waktuTutupJumat = Carbon::parse($waktuJumat->tutup);
     $user = User::findOrFail(auth('web')->id());
     $presensiUserSekarang = $user->presensi->where('tanggal', '=', now()->toDateString())->first();
-    $jamPresensiTutup = Carbon::parse($waktu->tutup);
-
-    return view('presensi', compact('user', 'presensiUserSekarang', 'jamPresensiTutup'));
+    
+    return view('presensi', compact(
+      'user', 
+      'presensiUserSekarang', 
+      'waktuBukaBiasa', 
+      'waktuTutupBiasa',
+      'waktuTutupJumat'
+    ));
   }
 
   public function prosesPresensiDatang(Request $request) {
     $user = User::findOrFail(auth('web')->id());
     
     function cekStatus($requestJamMasuk) {
-      $waktu = Waktu::findOrFail(1);
-      $jamPresensiBuka = Carbon::parse($waktu->buka);
-      $jamPresensiTutup = Carbon::parse($waktu->tutup);
       $jamMasuk = Carbon::parse($requestJamMasuk);
+      $waktuBiasa = Waktu::where('hari', 'biasa')->first();
+      $waktuBukaBiasa = Carbon::parse($waktuBiasa->buka);
+      $waktuTutupBiasa = Carbon::parse($waktuBiasa->tutup);
+      $waktuJumat = Waktu::where('hari', 'jumat')->first();
+      $waktuBukaJumat = Carbon::parse($waktuBiasa->buka);
+      $waktuTutupJumat = Carbon::parse($waktuBiasa->tutup);
 
-      // jam masuk antara jam 6 - 7
-      if($jamMasuk <= $jamPresensiBuka->subHour(1)) {
-        return $status = 'hadir';
-      } elseif($jamMasuk->between($jamPresensiBuka, $jamPresensiTutup)) {
-        return $status = 'terlambat';
+      if(strtolower(now()->isoFormat('dddd')) == 'jumat') {
+        if($jamMasuk->between($waktuBukaJumat->copy()->subHour(1), $waktuBukaJumat)) {
+          return $status = 'hadir';
+        } elseif($jamMasuk->between($waktuBukaJumat, $waktuTutupJumat->copy()->subHour(1))) {
+          return $status = 'terlambat';
+        }
       } else {
-        return $status = null;
+        if($jamMasuk->between($waktuBukaBiasa->copy()->subHour(1), $waktuBukaBiasa)) {
+          return $status = 'hadir';
+        } elseif($jamMasuk->between($waktuBukaBiasa, $waktuTutupBiasa->copy()->subHour(1))) {
+          return $status = 'terlambat';
+        }
       }
     }
     
--- resources/views/presensi.blade.php
+++ resources/views/presensi.blade.php
@@ -2,129 +2,177 @@
 @section('content')
 <div>
   <div class="row">
-    <!-- presensi datang -->
-    <div class="col-sm-8 col-md-8 col-xl-5">
-      <div class="card mb-4">
-        <div class="card-header fs-5">
-          Presensi Datang
-        </div>
-        <div class="card-body">
-          <div class="row text-capitalize">
-            <div class="col-12 col-md-6">
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Nama</span>
-                <span>{{ $user->username }}</span>
+    @if(strtolower(now()->isoFormat('dddd')) != 'sabtu' || strtolower(now()->isoFormat('dddd')) != 'minggu')
+      <!-- presensi datang -->
+      <div class="col-sm-8 col-md-8 col-xl-5">
+        <div class="card mb-4">
+          <div class="card-header fs-5">
+            Presensi Datang
+          </div>
+          <div class="card-body">
+            <div class="row text-capitalize">
+              <div class="col-12 col-md-6">
+                <div class="d-flex flex-column mb-3">
+                  <span class="fs-6 fw-semibold">Nama</span>
+                  <span>{{ $user->username }}</span>
+                </div>
+                <div class="d-flex flex-column mb-3">
+                  <span class="fs-6 fw-semibold">Asal Sekolah</span>
+                  <span>{{ $user->asal_sekolah }}</span>
+                </div>
+                <div class="d-flex flex-column mb-3">
+                  <span class="fs-6 fw-semibold">Tanggal</span>
+                  <span>{{ now()->translatedFormat('D, d-m-Y') }}</span>
+                </div>
               </div>
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Asal Sekolah</span>
-                <span>{{ $user->asal_sekolah }}</span>
-              </div>
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Tanggal</span>
-                <span>{{ now()->translatedFormat('D, d-m-Y') }}</span>
-              </div>
+              <form action="{{ route('proses.presensi.datang', [auth('web')->id()]) }}" method="POST">
+                @csrf
+                <input type="hidden" name="tanggal" value="{{ now() }}">
+                <input type="hidden" name="jam_masuk" value="{{ now() }}">
+                {{-- jika hari jumat --}}
+                @if(strtolower(now()->isoFormat('dddd')) == 'jumat')
+                  <!-- jika dini hari - jam 6 pagi -->
+                  @if(now()->between(now()->startOfDay(), $waktuBukaBiasa->copy()->subHour(1)))
+                    <div class="alert alert-info fw-medium">
+                      Presensi Belum Dibuka
+                    </div>
+                  {{-- jika jam buka - tutup & jam masuk empty --}}
+                  @elseif(now()->between($waktuBukaBiasa->copy()->subHour(1), $waktuTutupJumat->copy()->subHour(1)) && empty($presensiUserSekarang->jam_masuk))
+                    <button type="submit" class="btn btn-primary">Presensi</button>
+                  {{-- jika jam buka - tutup & jam masuk isset --}}
+                  @elseif(now()->between($waktuBukaBiasa->copy()->subHour(1), $waktuTutupJumat->copy()->subHour(1)) && isset($presensiUserSekarang->jam_masuk))
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  {{-- jika jam tutup - malam & jam masuk empty --}}
+                  @elseif(now()->between($waktuTutupJumat->copy()->subHour(1), $waktuTutupJumat->copy()->endOfDay()) && empty($presensiUserSekarang->jam_masuk))
+                    <div class="alert alert-danger fw-medium">
+                      Anda Tidak Presensi
+                    </div>
+                  @else 
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  @endif
+                {{-- jika hari Biasa --}}
+                @else()
+                  {{-- jika jam buka - tutup & jam masuk empty --}}
+                  @if(now()->between($waktuBukaBiasa->copy()->subHour(1), $waktuTutupBiasa->copy()->subHour(1)) && empty($presensiUserSekarang->jam_masuk))
+                    <button type="submit" class="btn btn-primary">Presensi</button>
+                  {{-- jika jam buka - tutup & jam masuk isset --}}
+                  @elseif(now()->between($waktuBukaBiasa->copy()->subHour(1), $waktuTutupBiasa->copy()->subHour(1)) && isset($presensiUserSekarang->jam_masuk))
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  {{-- jika jam tutup - malam & jam masuk empty --}}
+                  @elseif(now()->between($waktuTutupBiasa->copy()->subHour(1), $waktuTutupBiasa->copy()->endOfDay()) && empty($presensiUserSekarang->jam_masuk))
+                    <div class="alert alert-danger fw-medium">
+                      Anda Tidak Presensi
+                    </div>
+                  @else
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  @endif
+                @endif
+              </form>
             </div>
-            <form action="{{ route('proses.presensi.datang', [auth('web')->id()]) }}" method="POST">
-              @csrf
-              <input type="hidden" name="tanggal" value="{{ now() }}">
-              <input type="hidden" name="jam_masuk" value="{{ now() }}">
-              <!-- cek statusnya di hari ini apakah ada -->
-              @if(empty($presensiUserSekarang->status))
-              <button type="submit" class="btn btn-primary">Presensi</button>
-              @else
-              <div class="alert alert-success d-flex align-items-center gap-2 py-2 fw-medium">
-                <i class="fs-2 bi bi-check2-circle"></i>
-                <span>Anda sudah presensi</span>
-              </div>
-              @endif
-            </form>
           </div>
         </div>
       </div>
-    </div>
-    <!-- /presensi datang -->
+      <!-- /presensi datang -->
 
-    @if(isset($presensiUserSekarang->jam_keluar))
-    <div class="col-sm-8 col-md-8 col-xl-5">
       <!-- presensi pulang -->
-      <div class="card">
-        <div class="card-header fs-5">
-          Presensi Pulang
-        </div>
-        <div class="card-body">
-          <div class="row text-capitalize">
-            <div class="col-12 col-md-6">
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Nama</span>
-                <span>{{ $user->username }}</span>
+      <div class="col-sm-8 col-md-8 col-xl-5">
+        <div class="card">
+          <div class="card-header fs-5">
+            Presensi Pulang
+          </div>
+          <div class="card-body">
+            <div class="row text-capitalize">
+              <div class="col-12 col-md-6">
+                <div class="d-flex flex-column mb-3">
+                  <span class="fs-6 fw-semibold">Nama</span>
+                  <span>{{ $user->username }}</span>
+                </div>
+                <div class="d-flex flex-column mb-3">
+                  <span class="fs-6 fw-semibold">Asal Sekolah</span>
+                  <span>{{ $user->asal_sekolah }}</span>
+                </div>
+                <div class="d-flex flex-column mb-3">
+                  <span class="fs-6 fw-semibold">Tanggal</span>
+                  <span>{{ now()->translatedFormat('D, d-m-Y') }}</span>
+                </div>
               </div>
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Asal Sekolah</span>
-                <span>{{ $user->asal_sekolah }}</span>
-              </div>
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Tanggal</span>
-                <span>{{ now()->translatedFormat('D, d-m-Y') }}</span>
-              </div>
+              <form action="{{ route('proses.presensi.pulang', [auth('web')->id()]) }}" method="POST">
+                @csrf
+                <input type="hidden" name="jam_keluar" value="{{ now() }}">
+
+                {{-- jika hari jumat --}}
+                @if(strtolower(now()->isoFormat('dddd')) == 'jumat')
+                  <!-- jika dini hari - jam tutup -->
+                  @if(now()->between(now()->startOfDay(), $waktuTutupJumat))
+                    <div class="alert alert-info fw-medium">
+                      Presensi Belum Dibuka
+                    </div>
+                  {{-- jika jam tutup - +3 & jam keluar empty --}}
+                  @elseif(now()->between($waktuTutupJumat, $waktuTutupJumat->copy()->addHour(3)) && empty($presensiUserSekarang->jam_keluar))
+                    <button type="submit" class="btn btn-primary">Presensi</button>
+                  {{-- jika jam tutup - +3 & jam keluar isset --}}
+                  @elseif(now()->between($waktuTutupJumat, $waktuTutupJumat->copy()->addHour(3)) && isset($presensiUserSekarang->jam_keluar))
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  {{-- jika jam tutup +3 - malam & jam keluar empty --}}
+                  @elseif(now()->between($waktuTutupJumat->copy()->addHour(3), $waktuTutupJumat->copy()->endOfDay()) && empty($presensiUserSekarang->jam_keluar))
+                    <div class="alert alert-danger fw-medium">
+                      Anda Tidak Presensi
+                    </div>
+                  @else 
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  @endif
+                {{-- jika hari Biasa --}}
+                @else()
+                  <!-- jika dini hari - jam tutup -->
+                  @if(now()->between(now()->startOfDay(), $waktuTutupBiasa))
+                    <div class="alert alert-info fw-medium">
+                      Presensi Belum Dibuka
+                    </div>
+                  {{-- jika jam tutup - +3 & jam keluar empty --}}
+                  @elseif(now()->between($waktuTutupBiasa, $waktuTutupBiasa->copy()->addHour(3)) && empty($presensiUserSekarang->jam_keluar))
+                    <button type="submit" class="btn btn-primary">Presensi</button>
+                  {{-- jika jam tutup - +3 & jam keluar isset --}}
+                  @elseif(now()->between($waktuTutupBiasa, $waktuTutupBiasa->copy()->addHour(3)) && isset($presensiUserSekarang->jam_keluar))
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  {{-- jika jam tutup +3 - malam & jam keluar empty --}}
+                  @elseif(now()->between($waktuTutupBiasa->copy()->addHour(3), now()->endOfDay()) && empty($presensiUserSekarang->jam_keluar))
+                    <div class="alert alert-danger fw-medium">
+                      Anda Tidak Presensi
+                    </div>
+                  @else 
+                    <div class="alert alert-success fw-medium">
+                      Anda Sudah Presensi
+                    </div>
+                  @endif
+                @endif
+
+              </form>
             </div>
-            <form action="{{ route('proses.presensi.pulang', [auth('web')->id()]) }}" method="POST">
-              @csrf
-              <input type="hidden" name="jam_keluar" value="{{ now() }}">
-              @if(empty($presensiUserSekarang->jam_keluar))
-              <button type="submit" class="btn btn-primary">Presensi</button>
-              @else
-              <div class="alert alert-success d-flex align-items-center gap-2 py-2 fw-medium">
-                <i class="fs-2 bi bi-check2-circle"></i>
-                <span>Anda sudah presensi</span>
-              </div>
-              @endif
-            </form>
           </div>
         </div>
       </div>
       <!-- /presensi pulang -->
-    </div>
-    @elseif(now()->between($jamPresensiTutup->copy()->subHour(1), $jamPresensiTutup))
-    <div class="col-sm-8 col-md-8 col-xl-5">
-      <!-- presensi pulang -->
-      <div class="card">
-        <div class="card-header fs-5">
-          Presensi Pulang
-        </div>
-        <div class="card-body">
-          <div class="row text-capitalize">
-            <div class="col-12 col-md-6">
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Nama</span>
-                <span>{{ $user->username }}</span>
-              </div>
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Asal Sekolah</span>
-                <span>{{ $user->asal_sekolah }}</span>
-              </div>
-              <div class="d-flex flex-column mb-3">
-                <span class="fs-6 fw-semibold">Tanggal</span>
-                <span>{{ now()->translatedFormat('D, d-m-Y') }}</span>
-              </div>
-            </div>
-            <form action="{{ route('proses.presensi.pulang', [auth('web')->id()]) }}" method="POST">
-              @csrf
-              <input type="hidden" name="jam_keluar" value="{{ now() }}">
-              @if(empty($presensiUserSekarang->jam_keluar))
-              <button type="submit" class="btn btn-primary">Presensi</button>
-              @else
-              <div class="alert alert-success d-flex align-items-center gap-2 py-2 fw-medium">
-                <i class="fs-2 bi bi-check2-circle"></i>
-                <span>Anda sudah presensi</span>
-              </div>
-              @endif
-            </form>
+      @else
+        <div class="col-sm-8 col-md-8 col-xl-5">
+          <div class="alert alert-info fw-medium">
+            Presensi Tutup
           </div>
         </div>
-      </div>
-      <!-- /presensi pulang -->
-    </div>
-    @endif
+      @endif
   </div>
 </div>
 @endsection
